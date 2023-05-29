01b0ff3ca6ffcf3364997c1b629387128a4fee83
christykazi <christyagbai4178@gmail.com>
2023-05-29 02:10:58 +0100

    my birthday app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import List from './List';
+import date from './date';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ 
+ const App = () => {
 
-export default App;
+  const [people,setPeople] =useState(date);
+   return (
+     
+     <main>
+      <section className="container">
+        <h3> {people.length} birthday today</h3>
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>Clear All</button>
+        
+      </section>
+     </main>
+   )
+ }
+ 
+ export default App
